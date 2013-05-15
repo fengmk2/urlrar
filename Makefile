ded0e5994448f76146c4117c127195e3dca1df29
fengmk2 <fengmk2@gmail.com>
2013-05-15 15:20:42 +0800

    add jscover

--- Makefile
+++ Makefile
@@ -1,10 +1,24 @@
-SRC = $(shell find lib -type f -name "*.js")
-TESTS = test/*.js
-TESTTIMEOUT = 5000
+TESTS = test/*.test.js
 REPORTER = spec
+TIMEOUT = 10000
+JSCOVERAGE = ./node_modules/jscover/bin/jscover
+
+install:
+	@npm install
 
 test:
-	@NODE_ENV=test ./node_modules/.bin/mocha \
-		--reporter $(REPORTER) --timeout $(TESTTIMEOUT) $(TESTS)
+	@NODE_ENV=test ./node_modules/mocha/bin/mocha \
+		--reporter $(REPORTER) \
+		--timeout $(TIMEOUT) \
+		$(TESTS)
 
-.PHONY: test
+test-cov: cov
+	@URLRAR_COV=1 $(MAKE) -C .cov test
+	@URLRAR_COV=1 $(MAKE) -C .cov test REPORTER=html-cov > coverage.html
+
+cov:
+	@rm -rf .cov
+	@$(JSCOVERAGE) . .cov
+	@cp -rf node_modules .cov
+
+.PHONY: test-cov test cov
--- test/urllib.test.js
+++ test/urllib.test.js
@@ -3,37 +3,48 @@
  */
 
 var urllib = require('../lib/urllib');
-var should = require('../node_modules/should');
+var should = require('should');
 var http = require('http');
+var pedding = require('pedding');
 
-describe('urllib', function() {
-  var mapping = [ 
-    [ 'http://www.baidu.com/', 'http://www.baidu.com/' ],
-    [ 'http://t.cn/StVkqS', 'http://nodejs.org/community/' ],
-    [ 'http://url.cn/48JGfK', 'http://baike.baidu.com/view/6341048.htm' ],
-    [ 'http://t.cn/aK1IFu', 'http://v.youku.com/v_show/id_XMjc2MjY1NjEy.html' ],
-    [ 'http://url.cn/3OMI3O', 'http://v.youku.com/v_show/id_XMjc2MjY1NjEy.html', 2 ], // 2 times redirect
-    [ 'http://luo.bo/17221/', 'http://luo.bo/17221/' ],
-    [ 'http://t.itc.cn/LLHD6', 'http://app.chrome.csdn.net/work_detail.php?id=57' ],
-  ];
+
+describe('urllib.test.js', function() {
   
-  describe('#expand()', function() {
+  describe('expand()', function () {
+    var mapping = [ 
+      [ 'http://www.baidu.com/', 'http://www.baidu.com/', 0 ],
+      [ 'http://163.fm/7yYSdlI', 'http://nodejs.org/community/', 2 ],
+      [ 'http://t.cn/zlFTepd', 'http://w775.51qiangzuo.com/', 1 ],
+      [ 'http://luo.bo/17221/', 'http://luo.bo/17221/', 0 ],
+      [ 'http://t.itc.cn/LLHD6', 'http://app.chrome.csdn.net/work_detail.php?id=57', 1 ],
+    ];
     var desc = 'should expand ' + mapping.length + ' shorten urls success';
-    // it(desc, function(done) {
-    //   var counter = 0;
-    //   mapping.forEach(function(map) {
-    //     urllib.expand(map[0], function(err, longurl, redirectCounter) {
-    //       should.not.exist(err);
-    //       longurl.should.equal(map[1]);
-    //       if (map[2]) {
-    //         redirectCounter.should.equal(map[2]);
-    //       }
-    //       if (++counter === mapping.length) {
-    //         done();
-    //       }
-    //     })
-    //   })
-    // });
+
+    it(desc, function (done) {
+      var counter = 0;
+      mapping.forEach(function (map) {
+        urllib.expand(map[0], function (err, longurl, redirectCounter) {
+          should.not.exist(err);
+          longurl.should.equal(map[1]);
+          redirectCounter.should.equal(map[2]);
+          if (++counter === mapping.length) {
+            done();
+          }
+        })
+      })
+    });
+
+    it(desc + ' with pedding', function (done) {
+      done = pedding(mapping.length, done);
+      mapping.forEach(function (map) {
+        urllib.expand(map[0], function (err, longurl, redirectCounter) {
+          should.not.exist(err);
+          longurl.should.equal(map[1]);
+          redirectCounter.should.equal(map[2]);
+          done();
+        })
+      })
+    });
 
     it('should return empty string when shorturl set wrong', function(done) {
       urllib.expand('', function(err, longurl) {
